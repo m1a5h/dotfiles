7135f684167d67f45723554e04e1fbbc24a24096
Matthew Ashman <matthewiashman@gmail.com>
2020-08-23 20:54:10 +0100

    Add extra Marmalade and GNU repos (commented out the former as certs are expired)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,9 +5,15 @@
 ;; Enables basic packaging support
 (require 'package)
 (setq package-enable-at-startup nil)
-;; Adds the Melpa archive to the list of available repositories
+;; Adds the Melpa, Marmalade and GNU archive to the list of available repositories
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+;; Commenting out Marmalade (for now at least) at its certs are expired
+;; (add-to-list 'package-archives
+;; 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/") t)
+
 ;; Initializes the package infrastructure
 (package-initialize)
 ;; If there are no archived package contents, refresh them
